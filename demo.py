06c92059e2da82e69aa4bde978fea5aa36c59094
Suarim <muhammadsuarim@gmail.com>
2024-09-16 23:13:58 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,15 @@
+"""
+This module defines a simple function that returns a fixed value.
+"""
+
 def func():
+    """
+    Returns a fixed integer value.
+
+    Returns:
+        int: The integer value 8.
+    """
     a = 8
     return a
-
 
 func()
